d02f29bcd02b3fb164bb85bbea53674093f06b7e
Krister Emren <criz_81@yahoo.com>
2022-01-10 16:54:15 +0100

    Added keys for Back4App

--- parse-server/index.js
+++ parse-server/index.js
@@ -4,11 +4,11 @@ var ParseServer = require('parse-server').ParseServer;
 
 var app = express();
 var admin = new ParseServer({
-  databaseURI: 'mongodb://127.0.0.1:27017/parse',
+  databaseURI: 'mongodb://admin:IX2zH7D3DGmkp8wEel1rte3Y@MongoS3601A.back4app.com:27017/0936202593d54bb2be2566adbbea1000',
   cloud: './cloud/main.js',
-  appId: 'WebVote',
-
-  masterKey: 'WebVoteMasterKey'
+  appId: 'VavejCBuFbWSwByr9TY4PpKjheKBbH2EEMxaT9sQ',
+  javascriptKey: '4lY78FyWcoy9ctT6JCWqakt9BtXxQAzGO2zqJz23',
+  masterKey: 'NvVKghs2I2O9zC9twmvZTo4roP01JvNJ6zprrOuf'
 });
 
 // Serve the Parse API at /parse URL prefix
--- web-vote-voter/src/index.js
+++ web-vote-voter/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 
 
 const Parse = require('parse');
-Parse.initialize("WebVote");
-Parse.serverURL = 'http://localhost:1337/parse';
+Parse.initialize('VavejCBuFbWSwByr9TY4PpKjheKBbH2EEMxaT9sQ', '4lY78FyWcoy9ctT6JCWqakt9BtXxQAzGO2zqJz23');
+Parse.serverURL = 'https://parseapi.back4app.com/';
 
 ReactDOM.render(
   <React.StrictMode>
